2ad849c5b709c75d8e7a1c1320e51bdf69be5b0f
prashant-022a <pkumarofficial02@gmail.com>
2024-01-29 00:26:42 +0530

    user app & prediction added

--- src/cnnClassifier/pipeline/stage_03_training.py
+++ src/cnnClassifier/pipeline/stage_03_training.py
@@ -33,5 +33,6 @@ if __name__ == '__main__':
         logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")   
         
     except Exception as e:
+        logger.exception(e)
         raise e
         
