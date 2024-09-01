14e6c3df147a37b1f0dac45bdaea2862444f8a8b
prashant-022a <pkumarofficial02@gmail.com>
2024-09-01 14:51:37 +0530

    test comment

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,10 +1,12 @@
 from cnnClassifier.constants import *
 import os
+from pathlib import Path
 from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
                                                 PrepareCallbacksConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,
+                                                EvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -67,22 +69,6 @@ class ConfigurationManager:
         return prepare_callback_config
     
     
-    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
-        config = self.config.prepare_callbacks
-        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
-        create_directories([
-            Path(model_ckpt_dir),
-            Path(config.tensorboard_root_log_dir)
-        ])
-        
-        prepare_callback_config = PrepareCallbacksConfig(
-            root_dir = Path(config.root_dir),
-            tensorboard_root_log_dir = Path(config.tensorboard_root_log_dir),
-            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
-        )
-        
-        return prepare_callback_config
-    
     def get_training_config(self) -> TrainingConfig:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
@@ -104,3 +90,16 @@ class ConfigurationManager:
         )
         
         return training_config
+    
+    
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        
+        return eval_config
